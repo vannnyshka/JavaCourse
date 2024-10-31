dbbdd7de120166aaa6c4d949436f325512089fa7
Hanna Zdanevich <77280695+vannnyshka@users.noreply.github.com>
2024-10-31 14:23:10 +0300

    lesson4 computer finished

---
+++ src/lesson4/computer/HardDrive.java
@@ -0,0 +1,21 @@
+package lesson4.computer;
+
+public class HardDrive {
+    private String name;
+
+    public HardDrive(){
+
+    }
+
+    public HardDrive(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
