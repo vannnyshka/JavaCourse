308cd4ca3b1be35661a6bf3b46fc6c52428e957b
Hanna Zdanevich <77280695+vannnyshka@users.noreply.github.com>
2024-11-05 12:56:04 +0300

    String methods completed

---
+++ src/lesson4/fio/Person.java
@@ -0,0 +1,93 @@
+package lesson4.fio;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private String surname;
+    private String patronymic;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public Person(String name, String surname, String patronymic) {
+        this.name = name;
+        this.surname = surname;
+        this.patronymic = patronymic;
+    }
+
+    public Person(String string) {
+        string = string.trim();
+
+        int countSpace = 0;
+
+        for (int i = 0; i < string.length(); i++) {
+
+            if (string.charAt(i) == ' ') {
+                countSpace += 1;
+            }
+        }
+
+        if (countSpace != 2) {
+            System.out.println("ФИО должно быть в формате Фамилия Имя Отчество");
+        } else {
+            int spaceIndexOne = string.indexOf(' ');
+            int spaceIndexTwo = string.indexOf(' ', spaceIndexOne + 1);
+
+            String surname = string.substring(0, spaceIndexOne).trim();
+            String name = string.substring(spaceIndexOne, spaceIndexTwo).trim();
+            String patronymic = string.substring(spaceIndexTwo).trim();
+            this.name = name;
+            this.surname = surname;
+            this.patronymic = patronymic;
+        }
+    }
+
+    public Person() {
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(name, person.name) && Objects.equals(surname, person.surname) && Objects.equals(patronymic, person.patronymic);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, patronymic);
+    }
+
+
+}
---
+++ src/lesson4/string/Main.java
@@ -0,0 +1,25 @@
+package lesson4.string;
+
+public class Main {
+    public static void main(String[] args) {
+        String alphabetRus = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+        String alphabetEng = "abcdefghIjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        System.out.println("RUSSIAN ALPHABET: ");
+        for (int i = 0; i < alphabetRus.length(); i++) {
+            System.out.println(alphabetRus.charAt(i) + ": " + (int) alphabetRus.charAt(i));
+        }
+
+        System.out.println("ENGLISH ALPHABET: ");
+        for (int i = 0; i < alphabetEng.length(); i++) {
+            System.out.println(alphabetEng.charAt(i) + ": " + (int) alphabetEng.charAt(i));
+        }
+
+
+        System.out.println("RUSSIAN ALPHABET: ");
+        for (int i = 'А'; i <= 'я'; i++) {
+            System.out.println((char) i + ": " + i);
+        }
+
+    }
+}
